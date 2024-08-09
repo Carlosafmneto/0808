ec08c66e105d9414d1efbc793da78af9163820b2
Caelosafmneto <carlos.afmneto@senacsp.edu.br>
2024-08-09 19:45:51 -0300

    0808 tabela

---
+++ TI45-08-08-2024/Fornecedores.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace TI45_08_08_2024
+{
+    partial class Fornecedores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtContato = new System.Windows.Forms.TextBox();
+            this.lblContato = new System.Windows.Forms.Label();
+            this.txtCNPJ = new System.Windows.Forms.TextBox();
+            this.lblCNPJ = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.bntIncluir = new System.Windows.Forms.Button();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtContato);
+            this.groupBox1.Controls.Add(this.lblContato);
+            this.groupBox1.Controls.Add(this.txtCNPJ);
+            this.groupBox1.Controls.Add(this.lblCNPJ);
+            this.groupBox1.Controls.Add(this.btnFechar);
+            this.groupBox1.Controls.Add(this.btnExcluir);
+            this.groupBox1.Controls.Add(this.btnAlterar);
+            this.groupBox1.Controls.Add(this.bntIncluir);
+            this.groupBox1.Controls.Add(this.txtCidade);
+            this.groupBox1.Controls.Add(this.lblCidade);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Controls.Add(this.lblNome);
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Controls.Add(this.lblId);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(509, 266);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " CadBásicoFornecedores";
+            // 
+            // txtContato
+            // 
+            this.txtContato.Location = new System.Drawing.Point(84, 162);
+            this.txtContato.Name = "txtContato";
+            this.txtContato.Size = new System.Drawing.Size(133, 20);
+            this.txtContato.TabIndex = 13;
+            // 
+            // lblContato
+            // 
+            this.lblContato.AutoSize = true;
+            this.lblContato.Location = new System.Drawing.Point(17, 165);
+            this.lblContato.Name = "lblContato";
+            this.lblContato.Size = new System.Drawing.Size(44, 13);
+            this.lblContato.TabIndex = 12;
+            this.lblContato.Text = "Contato";
+            // 
+            // txtCNPJ
+            // 
+            this.txtCNPJ.Location = new System.Drawing.Point(84, 127);
+            this.txtCNPJ.Name = "txtCNPJ";
+            this.txtCNPJ.Size = new System.Drawing.Size(133, 20);
+            this.txtCNPJ.TabIndex = 11;
+            // 
+            // lblCNPJ
+            // 
+            this.lblCNPJ.AutoSize = true;
+            this.lblCNPJ.Location = new System.Drawing.Point(17, 130);
+            this.lblCNPJ.Name = "lblCNPJ";
+            this.lblCNPJ.Size = new System.Drawing.Size(34, 13);
+            this.lblCNPJ.TabIndex = 10;
+            this.lblCNPJ.Text = "CNPJ";
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(343, 211);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 9;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(234, 211);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 8;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(125, 211);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 7;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // bntIncluir
+            // 
+            this.bntIncluir.Location = new System.Drawing.Point(20, 211);
+            this.bntIncluir.Name = "bntIncluir";
+            this.bntIncluir.Size = new System.Drawing.Size(75, 23);
+            this.bntIncluir.TabIndex = 6;
+            this.bntIncluir.Text = "Incluir";
+            this.bntIncluir.UseVisualStyleBackColor = true;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(84, 91);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(225, 20);
+            this.txtCidade.TabIndex = 5;
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Location = new System.Drawing.Point(17, 94);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(40, 13);
+            this.lblCidade.TabIndex = 4;
+            this.lblCidade.Text = "Cidade";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(84, 55);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(225, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(17, 58);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(84, 23);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(32, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(17, 23);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(16, 13);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id";
+            // 
+            // Fornecedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Fornecedores";
+            this.Text = "Fornecedores";
+            this.Load += new System.EventHandler(this.Fornecedores_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtContato;
+        private System.Windows.Forms.Label lblContato;
+        private System.Windows.Forms.TextBox txtCNPJ;
+        private System.Windows.Forms.Label lblCNPJ;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button bntIncluir;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblId;
+    }
+}
---
+++ TI45-08-08-2024/Produtos.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace TI45_08_08_2024
+{
+    partial class Produtos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEstoque = new System.Windows.Forms.TextBox();
+            this.lblEstoque = new System.Windows.Forms.Label();
+            this.txtEstabelecimento = new System.Windows.Forms.TextBox();
+            this.lblCEP = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.bntIncluir = new System.Windows.Forms.Button();
+            this.txtLocalização = new System.Windows.Forms.TextBox();
+            this.lblLocalização = new System.Windows.Forms.Label();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.lblItem = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtEstoque);
+            this.groupBox1.Controls.Add(this.lblEstoque);
+            this.groupBox1.Controls.Add(this.txtEstabelecimento);
+            this.groupBox1.Controls.Add(this.lblCEP);
+            this.groupBox1.Controls.Add(this.btnFechar);
+            this.groupBox1.Controls.Add(this.btnExcluir);
+            this.groupBox1.Controls.Add(this.btnAlterar);
+            this.groupBox1.Controls.Add(this.bntIncluir);
+            this.groupBox1.Controls.Add(this.txtLocalização);
+            this.groupBox1.Controls.Add(this.lblLocalização);
+            this.groupBox1.Controls.Add(this.txtItem);
+            this.groupBox1.Controls.Add(this.lblItem);
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Controls.Add(this.lblId);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(509, 266);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " CadBásicoProduto";
+            // 
+            // txtEstoque
+            // 
+            this.txtEstoque.Location = new System.Drawing.Point(108, 90);
+            this.txtEstoque.Name = "txtEstoque";
+            this.txtEstoque.Size = new System.Drawing.Size(133, 20);
+            this.txtEstoque.TabIndex = 13;
+            // 
+            // lblEstoque
+            // 
+            this.lblEstoque.AutoSize = true;
+            this.lblEstoque.Location = new System.Drawing.Point(17, 93);
+            this.lblEstoque.Name = "lblEstoque";
+            this.lblEstoque.Size = new System.Drawing.Size(46, 13);
+            this.lblEstoque.TabIndex = 12;
+            this.lblEstoque.Text = "Estoque";
+            // 
+            // txtEstabelecimento
+            // 
+            this.txtEstabelecimento.Location = new System.Drawing.Point(108, 161);
+            this.txtEstabelecimento.Name = "txtEstabelecimento";
+            this.txtEstabelecimento.Size = new System.Drawing.Size(133, 20);
+            this.txtEstabelecimento.TabIndex = 11;
+            // 
+            // lblCEP
+            // 
+            this.lblCEP.AutoSize = true;
+            this.lblCEP.Location = new System.Drawing.Point(17, 164);
+            this.lblCEP.Name = "lblCEP";
+            this.lblCEP.Size = new System.Drawing.Size(28, 13);
+            this.lblCEP.TabIndex = 10;
+            this.lblCEP.Text = "CEP";
+            this.lblCEP.Click += new System.EventHandler(this.lblCPF_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(343, 211);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 9;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(234, 211);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 8;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(125, 211);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 7;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // bntIncluir
+            // 
+            this.bntIncluir.Location = new System.Drawing.Point(20, 211);
+            this.bntIncluir.Name = "bntIncluir";
+            this.bntIncluir.Size = new System.Drawing.Size(75, 23);
+            this.bntIncluir.TabIndex = 6;
+            this.bntIncluir.Text = "Incluir";
+            this.bntIncluir.UseVisualStyleBackColor = true;
+            // 
+            // txtLocalização
+            // 
+            this.txtLocalização.Location = new System.Drawing.Point(108, 125);
+            this.txtLocalização.Name = "txtLocalização";
+            this.txtLocalização.Size = new System.Drawing.Size(225, 20);
+            this.txtLocalização.TabIndex = 5;
+            // 
+            // lblLocalização
+            // 
+            this.lblLocalização.AutoSize = true;
+            this.lblLocalização.Location = new System.Drawing.Point(17, 128);
+            this.lblLocalização.Name = "lblLocalização";
+            this.lblLocalização.Size = new System.Drawing.Size(64, 13);
+            this.lblLocalização.TabIndex = 4;
+            this.lblLocalização.Text = "Localização";
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(108, 55);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(225, 20);
+            this.txtItem.TabIndex = 3;
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Location = new System.Drawing.Point(17, 58);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(27, 13);
+            this.lblItem.TabIndex = 2;
+            this.lblItem.Text = "Item";
+            this.lblItem.Click += new System.EventHandler(this.lblNome_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(108, 23);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(32, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(17, 23);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(16, 13);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id";
+            // 
+            // Produtos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Produtos";
+            this.Text = "Produtos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtEstoque;
+        private System.Windows.Forms.Label lblEstoque;
+        private System.Windows.Forms.TextBox txtEstabelecimento;
+        private System.Windows.Forms.Label lblCEP;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button bntIncluir;
+        private System.Windows.Forms.TextBox txtLocalização;
+        private System.Windows.Forms.Label lblLocalização;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblId;
+    }
+}
